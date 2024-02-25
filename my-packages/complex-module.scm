d8cc79107c14e6112aa9837152a3ce6363d85020
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-02-25 20:06:17 +0100

    Finished to add comments to explain each part of the package build. Also removed a temporary file from emacs.

--- my-packages/complex-module.scm
+++ my-packages/complex-module.scm
@@ -21,6 +21,8 @@
    (name "custom-complex-libgit2")
    (version (git-version "0.26.6" revision commit))
    ;;(version (git-version "1.7.2" revision commit)) ;; latest on the 24.02.24
+
+   ;; SOURCE
    (source
     (origin
      (method git-fetch) ;; git-fetch expects url + commit
@@ -32,38 +34,67 @@
      (sha256 (base32 "17pjvprmdrx4h6bb1hhc98w9qi6ki7yl57f090n9kbhswxqfs7s3"))
      ;; (sha256 (base32 "0i95jwrwx4svh5l4dpa5r4a99f813hlm7nzzkbqzmnw4pkyxhlvx")) ;; for v1.7.2 obtained with :
 #!
-git clone https://github.com/libgit2/libgit2/
-cd libgit2
-git checkout v1.7.2
-guix hash -rx .     
+	;; git clone https://github.com/libgit2/libgit2/
+	;; cd libgit2
+	;; git checkout v1.7.2
+	;; guix hash -rx .     
 !#
      ;;
      (patches (search-patches "libgit2-mtime-0.patch"))
      (modules '((guix build utils)))
-     ;; Remove bundled software.
-     (snippet '(delete-file-recursively "deps"))))
+     
+     ;; SNIPPETS
+     ;; The below snippet is quoted (i.e non-evaluated) - it is an alternative to the '.patch' files and the code will be evaluated ONLY when passed to the guix daemon for building.
+     (snippet '(delete-file-recursively "deps")))) ;; Remove bundled software.
+
+   ;; OUTPUTS
+   ;; Each output is a directory in the /gnu/store.
+   (outputs '("out" "debug")) ;; "out" is the complete package
+
+   ;; BUILD SYSTEM ARGUMENTS
    (build-system cmake-build-system)
-   (outputs '("out" "debug"))
-   ;;
-   ;; SNIPPETS
+   ;; 'arguments' : keyword-value list used to configure the build process.
    (arguments
-    ;; The below snippet is quoted (i.e non-evaluated) - it is an alternative to the '.patch' files and the code will be evaluated ONLY when passed to the guix daemon for building.
-    `(#:tests? #true                         ; Run the test suite (this is the default)
-      #:configure-flags '("-DUSE_SHA1DC=ON") ; SHA-1 collision detection
-      #:phases
-      (modify-phases %standard-phases
+    `(#:tests? #true                         ;; Able/disable the test suite when building the package. Default to true.
+
+      ;; Reminder : usually, a package is build with :
+      ;; ./configure && make && make install' - for more details see : https://stackoverflow.com/questions/10961439/why-always-configure-make-make-install-as-3-separate-steps
+      
+      #:configure-flags '("-DUSE_SHA1DC=ON") ;; Set the flag for './configure'
+      ;; Here : SHA-1 collision detection
+      
+      ;; #:make-flags ;; Set the flag for 'make'. Here, no flag is specified.
+
+      ;; BUILD PHASES
+      #:phases ;; List the sequential steps of the build system
+      ;; Typically phases include unpack, configure, build, install and check.
+      ;; To know more about those phases, you need to work out the appropriate build system definition in ‘$GUIX_CHECKOUT/guix/build/gnu-build-system.scm’
+      (modify-phases %standard-phases ;; Customize the standard-phases
+#!
+      ;; Those are the available macro :
+      ;; (add-before phase new-phase procedure): Run procedure named new-phase before phase.
+      ;; (add-after phase new-phase procedure): Same, but afterwards.
+      ;; (replace phase procedure)
+      ;; (delete phase)
+
+      ;; The 'procedures' support the keywords arguments 'inputs' and 'outputs' - see cookbook for examples.
+      ;; The procedure must return #true on success.
+!#
+       
        (add-after 'unpack 'fix-hardcoded-paths
         (lambda _
-  	 (substitute* "tests/repo/init.c" (("#!/bin/sh") (string-append "#!" (which "sh"))))       
+  	 (substitute* "tests/repo/init.c" (("#!/bin/sh") (string-append "#!" (which "sh")))) ;; substitute* = sed-like function       
          (substitute* "tests/clar/fs.h" (("/bin/cp") (which "cp")) (("/bin/rm") (which "rm")))))
+
        ;; Run checks more verbosely.
        (replace 'check
         (lambda*
 	 (#:key tests? #:allow-other-keys)
          (when tests? (invoke "./libgit2_clar" "-v" "-Q"))))
+       
        (add-after 'unpack 'make-files-writable-for-tests
 		  (lambda _ (for-each make-file-writable (find-files ".")))))))
-   ;;
+
    ;; INPUTS
    ;; 'inputs' : Installed in the store but not in the profile, as well as being present at build time.
    (inputs (list libssh2 http-parser python-wrapper))
@@ -71,11 +102,9 @@ guix hash -rx .
    (native-inputs (list pkg-config))
    ;; 'propagated-inputs' : Installed in the store and in the profile, as well as being present at build time.
    (propagated-inputs (list openssl zlib)) ;; These two libraries are in 'Requires.private' in libgit2.pc.
-   ;; OUTPUTS
-   ;; Each output is a directory in the /gnu/store.
-   ;; No outputs here.
+
+   ;; COMMON PACKAGE INFO
    (home-page "https://libgit2.github.com/")
    (synopsis "Library providing Git core methods")
    (description "Libgit2 is a portable, pure C implementation of the Git core methods provided as a re-entrant linkable library with a solid API, allowing you to write native speed custom Git applications in any language with bindings.")
-   ;; GPLv2 with linking exception
-   (license license:gpl2))))
+   (license license:gpl2)))) ;; GPLv2 with linking exception - to avoid names conflict with eventual packages named the same way as the licence
