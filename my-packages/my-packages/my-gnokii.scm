6822e2ae117acaf68d0602005588e1feb9d59184
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-03-12 09:20:08 +0100

     Built successfully - in test for installation.

--- my-packages/my-packages/my-gnokii.scm
+++ my-packages/my-packages/my-gnokii.scm
@@ -1,6 +1,7 @@
 (define-module (my-packages my-gnokii)
   #:use-module (gnu packages autotools) ; for libtool, automake (for aclocal)
   #:use-module (gnu packages base) ; for gnu-make (make)
+  #:use-module (gnu packages compression) ; for xz (to apply custom patch)
   #:use-module (gnu packages build-tools) ; for gnulib-tool
   #:use-module (gnu packages flex)
   #:use-module (gnu packages gcc)
@@ -8,7 +9,7 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages version-control) ; For git
   #:use-module (gnu packages wget)
-  #:use-module (guix build utils) ; for 'substitute' definition
+  #:use-module (guix build utils) ; for 'substitute' definition, and for 'set-path-environment-variable'
   #:use-module (guix build-system gnu)
   #:use-module (guix download)
   #:use-module (guix gexp) ; for regex in snippet
@@ -17,49 +18,10 @@
   #:use-module (ice-9 regex) ; for the 'match:substring' procedure 
 )
 
-#!    
-(define* (bootstrap #:key (bootstrap-scripts %bootstrap-scripts)
-                    #:allow-other-keys)
-  "If the code uses Autotools and \"configure\" is missing, run
-\"autoreconf\".  Otherwise do nothing."
-  ;; Note: Run that right after 'unpack' so that the generated files are
-  ;; visible when the 'patch-source-shebangs' phase runs.
-  (define (script-exists? file)
-    (and (file-exists? file)
-         (not (file-is-directory? file))))
-
-  (if (not (script-exists? "configure"))
-
-      ;; First try one of the BOOTSTRAP-SCRIPTS.  If none exists, and it's
-      ;; clearly an Autoconf-based project, run 'autoreconf'.  Otherwise, do
-      ;; nothing (perhaps the user removed or overrode the 'configure' phase.)
-      (let ((script (find script-exists? bootstrap-scripts)))
-        ;; GNU packages often invoke the 'git-version-gen' script from
-        ;; 'configure.ac' so make sure it has a valid shebang.
-        (false-if-file-not-found
-         (patch-shebang "build-aux/git-version-gen"))
-
-        (if script
-            (let ((script (string-append "./" script)))
-              (setenv "NOCONFIGURE" "true")
-              (format #t "running '~a'~%" script)
-              (if (executable-file? script)
-                  (begin
-                    (patch-shebang script)
-                    (invoke script))
-                  (invoke "sh" script))
-              ;; Let's clean up after ourselves.
-              (unsetenv "NOCONFIGURE"))
-            (if (or (file-exists? "configure.ac")
-                    (file-exists? "configure.in"))
-                (invoke "autoreconf" "-vif")
-                (format #t "no 'configure.ac' or anything like that, \
-doing nothing~%"))))
-      (format #t "GNU build system bootstrapping not needed~%")))
-!#
 #!
-;;LIST OF MISSING FILE TO INSTALL 
-       "codeset.m4" ; OK in gnulib
+;; LIST OF MISSING FILE TO INSTALL KEPT IN CASE A BUG OCCURS DURING THE INSTALLATION.
+;; SO FAR THE BUILD IS OK.
+       "codeset.m4" ; OK in gnulib - INSTALLED MANUALLY
        "gettext.m4" ; OK in gnulib
        "glibc21.m4" ; NOT OK not in gnulib so to be found
        "iconv.m4" ; OK in gnulib
@@ -71,10 +33,11 @@ doing nothing~%"))))
 !#
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;; DEFINING A FEW MISSING FILES MANUALLY BY COPY/PASTING THEIR CONTENT;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;; DEFINING MISSING FILES MANUALLY BY COPY/PASTING THEIR CONTENT ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define codeset-m4-content
-  "# codeset.m4 serial 5 (gettext-0.18.2)
+"# codeset.m4 serial 5 (gettext-0.18.2)
 dnl Copyright (C) 2000-2002, 2006, 2008-2014, 2016, 2019-2023 Free Software
 dnl Foundation, Inc.
 dnl This file is free software; the Free Software Foundation
@@ -103,30 +66,6 @@ AC_DEFUN([AM_LANGINFO_CODESET],
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;; END OF FILES CONTENT DEFINITION  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-#!
-           (lambda _
-	     (let (
-		   (script (string-append "./" "autogen.sh"))
-		  )
-              (setenv "NOCONFIGURE" "true")
-              (format #t "running '~a'~%" script)
-              (if (executable-file? script)
-		  ; If yes
-                  (begin
-                    (patch-shebang script)
-
-                     ;(substitute* "autogen.sh"
-                     ;  (("autoconf") (string-append "autoconf" "&&" "echo haha")))
-		    
-                    (invoke script))
-		  ; Else
-                  (invoke "sh" script))
-              ;; Let's clean up after ourselves.
-              (unsetenv "NOCONFIGURE"))
-	     )
-!#
-
 		 
 (define-public my-gnokii
   (package
@@ -140,14 +79,14 @@ AC_DEFUN([AM_LANGINFO_CODESET],
        (sha256
         (base32
          "1iphhjsz9v1pk0a3p2zq09k44x7pyg1a6m3dcpf6n0bpyfgskpsn"))
-      ; )
+       (modules '((guix build utils))) ; for the 'set-path-environment-variable' procedure
        (snippet
 	#~(begin
        ;; Adding the content of missing files manually by copy/pasting their content into string variable that I call
 	      (with-output-to-file (string-append "m4/codeset.m4")
 		(lambda* (#:key codeset-m4-content #:allow-other-keys)
 			 (format #t "~a~%" #+codeset-m4-content)))
-                ))))
+	      ))))
     
     (build-system gnu-build-system)
 
@@ -155,16 +94,13 @@ AC_DEFUN([AM_LANGINFO_CODESET],
     (arguments
      (list
       #:modules
-	`((ice-9 regex) ; for the 'match:substring' procedure 
+      `((ice-9 regex) ; for the 'match:substring' procedure
          ,@%gnu-build-system-modules)
-	    ;(guix build utils) (guix build-system gnu))
-         ;(guix build font-build-system))
        #:phases
        #~(modify-phases %standard-phases		
-        (replace 'bootstrap
-            (lambda* (#:key inputs #:allow-other-keys)
-		 (let* (
-			(script "./autogen.sh")
+            (replace 'bootstrap
+                (lambda* (#:key inputs #:allow-other-keys)
+		 (let* ((script "./autogen.sh")
 			(sh-path (which "sh"))
 			;; Get the list of each part of the path splitted by '/'
 			(list-of-path-parts (list (map match:substring (list-matches "[-\\.a-z0-9]+" sh-path))))
@@ -173,8 +109,7 @@ AC_DEFUN([AM_LANGINFO_CODESET],
 			(part-of-path-1 (list-ref (map match:substring (list-matches "[-\\.a-z0-9]+" sh-path)) 1))
 			(part-of-path-2 (list-ref (map match:substring (list-matches "[-\\.a-z0-9]+" sh-path)) 2))
 			(part-of-path-3 (list-ref (map match:substring (list-matches "[-\\.a-z0-9]+" sh-path)) 3))
-			(part-of-path-4 (list-ref (map match:substring (list-matches "[-\\.a-z0-9]+" sh-path)) 4))
-		  )
+			(part-of-path-4 (list-ref (map match:substring (list-matches "[-\\.a-z0-9]+" sh-path)) 4)))
 		;; Replacing the sheband in the configure file through the autoconf file by adding a replacement command with sed. Mandatory to be done here because before running the 'autogen.sh' script, the file 'configure' does not exist.
                 (substitute* '("autogen.sh")
 			     (("autoconf")
@@ -184,15 +119,25 @@ AC_DEFUN([AM_LANGINFO_CODESET],
 				  "\\/" part-of-path-2
 				  "\\/" part-of-path-3
 				  "\\/" part-of-path-4
-				  "/g' configure")) ; working
-			     )
+				  "/g' configure")))
 		 (patch-shebang script)		    
 		 (invoke script)
 	         ;; Let's clean up after ourselves.
                  (unsetenv "NOCONFIGURE") ; This line was in the 'bootstrap' definition so left it.
 		)
-	      )   
-	))))
+		 ))
+	    
+	    ;; IN DEVELOPMENT HERE : trying to replace a line in a file to avoid the bug 'local_atoi' is undefined
+	    ;; See : Patch from gnokii github : https://github.com/pld-linux/gnokii/blob/master/gnokii-gcc7.patch
+	    (add-after 'configure 'patch-gsm-file-to-avoid-local-atoi-error
+		       (lambda* (#:key inputs #:allow-other-keys) ; Test
+			;; Substitute* is by default in UTF-8 and the file was not - so issue while trying to replace the line.
+			(with-fluids ((%default-port-encoding "ISO-8859-1"))
+			(substitute* "common/gsm-filetypes.c"
+			     ((".*inline[[:space:]]int[[:space:]]local_atoi") "static int local_atoi")))
+		  ))
+
+	)))
 
     (inputs
      (list
@@ -208,7 +153,6 @@ AC_DEFUN([AM_LANGINFO_CODESET],
 	  intltool ; for 'intltoolize' in autogen.sh
 	  libtool
           pkg-config
-	  wget ; required for installing missing files : codeset.m4 gettext.m4 glibc21.m4 iconv.m4 isc-posix.m4 lcmessage.m4 progtest.m4
       )
      )
     (native-inputs '())
@@ -219,6 +163,5 @@ AC_DEFUN([AM_LANGINFO_CODESET],
     (home-page "https://gnokii.org/")
     (license license:gpl1)))
 
-;; Comment this line after finishing the development tests.
-my-gnokii
+;;my-gnokii ;; Uncomment this line during development tests. Comment it when finished.
 
