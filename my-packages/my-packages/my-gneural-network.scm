a0730bb94d0253c9618581d12ad0540e68c46114
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-03-14 18:15:11 +0100

    Created my own GitHub repo to host the package while I work on it. So adapt the package definition to take into account the github url, commit and hash. Build works so will install it.

--- my-packages/my-packages/my-gneural-network.scm
+++ my-packages/my-packages/my-gneural-network.scm
@@ -1,35 +1,34 @@
 (define-module (my-packages my-gneural-network)
   #:use-module (guix build-system gnu)
-  #:use-module (guix download) ; for the procedure 'url-fetch'
+  #:use-module (guix git-download) ; for 'git-fetch' procedure
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix packages) ; for 'package' definition 
+  #:use-module (guix packages) ; for 'package' procedure 
 )
- 		 
+
+;; At each commit, adust the commit and the hash below in order to download and build updated package.
 (define-public my-gneural-network
+  (let ((revision "1")
+    (commit "232786c69cd47b24d8ce6afcdd1441acd4607a64")) ; As of 240314
   (package
     (name "my-gneural-network")
     (version "0.9.1")
     (source
      (origin
-      (method url-fetch)
-      ; The package is not maintained anymore so cannot git download it.
-       (uri (string-append
-	     "https://cvs.savannah.gnu.org/viewvc/*checkout*/gneuralnetwork/gneuralnetwork/gneural_network-0.9.1.tar.gz"))
+      (method git-fetch)
+      (uri (git-reference
+	    (url "https://github.com/Pablo12345678901/gneural_network")
+	    (commit commit)))
        (sha256
-        (base32
-         "1qj3z1c4c88c9jq3sxrkhmgj4mpvzysllgakqimw9lfzv1dwj2yn"))
-       ))
-    
+        (base32 "1qzx2wvvsvzh3j5cb5k33pwz9g8kmfwgsri5dqgvw6cymfgaf9vn"))
+       (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (arguments '())
     (inputs '())
     (native-inputs '())
     (propagated-inputs '())
-    
     (synopsis "Gneural Network is the GNU package which implements a programmable neural network.")
     (description "Gneural Network is the GNU package which implements a programmable neural network.")
     (home-page "https://www.gnu.org/software/gneuralnetwork/")
-    (license license:gpl3)))
+    (license license:gpl3))))
 
-
-;;my-gneural-network  ;; Uncomment this line during development tests. Comment it when finished.
+;;my-gneural-network ; Uncomment this line during development tests. Comment it when finished.
