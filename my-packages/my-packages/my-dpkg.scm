2cb38829bbac3fcb61fbe03901ba657e2e8a27c8
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-03-23 07:34:31 +0100

     Comment a line that was used during build development test.

---
+++ my-packages/my-packages/my-dpkg.scm
@@ -0,0 +1,25 @@
+(define-module (my-packages my-dpkg)
+  #:use-module (gnu packages debian) ; for dpkg
+  #:use-module (guix build utils)
+  #:use-module (guix git-download)
+  #:use-module (guix packages) 
+)
+
+(define-public my-dpkg
+  (package
+    (inherit dpkg)
+    (name "dpkg")
+    (version "1.22.1")
+    (source
+     (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://git.dpkg.org/git/dpkg/dpkg")
+               (commit version)))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32 "1s6dzcczmpkr9pla25idymfdjz10gck0kphpp0vqbp92vmfskipg"))))
+
+))
+
+;;my-dpkg ;; Uncomment this line while developping / Re-comment it after.
